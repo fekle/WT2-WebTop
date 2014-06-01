b426b12854401b3b3393bbd6aed397e92be314c0
fekle <me@felixklein.at>
2014-06-01 11:46:21 -0400

    cleanup!

--- apps/fotos/upload.php
+++ apps/fotos/upload.php
@@ -3,33 +3,33 @@
 	//error reporting
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
-	
+
 	// common.php includen - da kommen die funktionen rein die immer gebraucht werden;
 	require_once("../../sys/common.php");
 	require_once("../../sys/objects.php");
-	
+
 	//session, ui und db starten
 	$session = new Session();
 	$db = new DB();
-	
+
 	//schaun ob user eingeloggt ist
 	$loggedIn = $session->loggedIn();
-	
+
 	$user = $db->userinfo($session->get("user"));
-	
+
 	$settings = $db->getSettings();
-	
+
 	$settings = $settings->apps->fotos;
-	
+
 	$uploads = $settings->uploads;
-	
+
 	require_once("../../templates/app_head.php");
 ?>
 <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
 <link href="fotos.css" rel="stylesheet">
 <link rel="stylesheet" href="js/jquery.plupload.queue/css/jquery.plupload.queue.css" type="text/css" media="screen" />
 <script type="text/javascript" src="js/plupload.full.min.js"></script>
-<script type="text/javascript" src="js/jquery.plupload.queue/jquery.plupload.queue.js"></script>
+<script type="text/javascript" src="js/jquery.plupload.queue.min.js"></script>
 
 <div id="foto_app_header">
 	Foto-App // Upload
@@ -58,7 +58,7 @@ $(function() {
 		url : 'get-upload.php',
 		chunk_size : 0,
 		unique_names : true,
-		
+
 		filters : {
 			max_file_size : '50mb',
 			mime_types: [
